8c8a9f4859d6163e0d54b1a66b34a149f0dc191d
AEmorales00 <albertoenriquem98@gmail.com>
2025-03-02 22:43:10 -0600

    Modificaciones Actuales

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.');
+    this.reset();
+});
